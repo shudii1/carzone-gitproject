c19ba71d2ed242ecc316641d2b6040146359f5f6
shudii1 <hustle162@gmail.com>
2021-02-01 23:27:40 +0530

    adding functionalities and break html

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def home(requsest):
     return render(requsest, 'pages/home.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def services(request):
+    return render(request, 'pages/services.html')
