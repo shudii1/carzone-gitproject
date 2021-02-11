5c1133802c48594a7002a51739e0e74478acdd30
shudii1 <hustle162@gmail.com>
2021-02-12 01:33:11 +0530

    customize search and admin

--- pages/views.py
+++ pages/views.py
@@ -8,13 +8,27 @@ from cars.models import Car
 def home(requsest):
     # fetch all data from database to teams
     teams = Team.objects.all()
+    # for showing featured car in the featured  car section
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars = Car.objects.order_by('-created_date')
+    # search_fields=Car.objects.values('model','city','year','body_style')
+
+    # for finding distinct values of modal,city,year,body_style in search (home)
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+
     # pass dictionary of model team to pass data of teams model in html using data dictionary
     data = {
         'teams': teams,
         'featured_cars': featured_cars,
         'all_cars': all_cars,
+        # 'search_fields':search_fields
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
     }
     return render(requsest, 'pages/home.html', data)
 
